a49449146bbb39f58ee24a51aeef396b68ebd566
Ald28 <aldopichihua123@gmail.com>
2026-01-31 07:35:22 -0500

    cambio en los campos de registro de extintor en swager

--- routes/nfc/create.routes.js
+++ routes/nfc/create.routes.js
@@ -20,10 +20,10 @@ const router = Router();
  *           schema:
  *             type: object
  *             properties:
- *               codeNFC:
+ *               codeExtintor:
  *                 type: string
  *                 example: "NFC123456"
- *               serialNumber:
+ *               serialNumberNFC:
  *                 type: string
  *                 example: "SERIE98765"
  *               type:
